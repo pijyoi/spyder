ca06895b23b7da539b91a5eaeb070f195f3b3e6a
KIU Shueng Chuan <nixchuan@gmail.com>
2019-02-17 21:10:44 +0800

    fix wrong type to setUnderLineStyle

--- spyder/utils/syntaxhighlighters.py
+++ spyder/utils/syntaxhighlighters.py
@@ -1047,7 +1047,7 @@ class MarkdownSH(BaseSH):
         self.formats['strikethrough'] = font
 
         font = QTextCharFormat(self.formats['string'])
-        font.setUnderlineStyle(True)
+        font.setUnderlineStyle(QTextCharFormat.SingleUnderline)
         self.formats['link'] = font
 
         self.formats['code'] = self.formats['string']
